df1e852fba5d0ca4fa6836f7d6d77bbf42f9eaca
davidnlearner <david.learner@gmail.com>
2016-10-08 16:56:31 -0400

    Update content.js

--- src/content.js
+++ src/content.js
@@ -7,3 +7,14 @@ function url_domain(data) {
 	var hostParts = dummy.hostname.split('.');
 	return hostParts[hostParts.length - 2] + '.' + hostParts[hostParts.length - 1];
 }
+function getMessage(userId, messageId, callback) {
+  var request = gapi.client.gmail.users.messages.get({
+    'userId': userId,
+    'id': messageId
+  });
+  request.execute(callback);
+}
+
+function callback(){
+	console.log("It worked" + Date.now());
+}
